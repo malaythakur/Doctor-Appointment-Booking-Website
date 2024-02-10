c26d9ed68aa96577ac1c7c03a21a5d342070fa6e
Malay Thakur <malaythakur13@gmail.com>
2024-02-10 20:52:09 +0530

    data of doctors updated

--- frontend/src/assets/data/doctors.js
+++ frontend/src/assets/data/doctors.js
@@ -5,8 +5,8 @@ import doctorImg03 from "../images/doctor-img03.png";
 export const doctors = [
   {
     id: "01",
-    name: "Dr. Alfaz Ahmed",
-    specialty: "Surgeon",
+    name: "Dr. Jorge Acosta",
+    specialization: "Surgeon",
     avgRating: 4.8,
     totalRating: 272,
     photo: doctorImg01,
@@ -15,8 +15,8 @@ export const doctors = [
   },
   {
     id: "02",
-    name: "Dr. Saleh Mahmud",
-    specialty: "Neurologist",
+    name: "Dr. Andrew Barreto",
+    specialization: "Neurologist",
     avgRating: 4.8,
     totalRating: 272,
     photo: doctorImg02,
@@ -25,8 +25,8 @@ export const doctors = [
   },
   {
     id: "03",
-    name: "Dr. Farid Uddin",
-    specialty: "Dermatologist",
+    name: "Dr. Alexander Berlin",
+    specialization: "Dermatologist",
     avgRating: 4.8,
     totalRating: 272,
     photo: doctorImg03,
