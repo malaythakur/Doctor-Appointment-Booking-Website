bacf902c8aaa4874ca3ed9d2c5084e99d22d25b7
Malay Thakur <malaythakur13@gmail.com>
2024-02-11 22:14:02 +0530

    Implemented Testimonial Section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -16,6 +16,7 @@ import About from "../components/About/About";
 import ServiceList from "../components/Services/ServiceList";
 import DoctorList from "../components/Doctors/DoctorList";
 import FaqList from "../components/Faq/FaqList";
+import Testimonial from "../components/Testimonial/Testimonial";
 
 const Home = () => {
     return (
@@ -341,6 +342,24 @@ const Home = () => {
 
         {/* =========== Our Faq Section End ========== */ }
 
+
+        {/* =========== Testimonial Start =========== */}
+
+        <section>
+            <div className="container">
+                <div className="xl:w-[470px] mx-auto">
+                    <h2 className="heading text-center"> What our patient say </h2>
+                        <p className="text__para text-center">
+                            World-class care for everyone. Our Health System offers unmatched,
+                            expert health care.
+                        </p>     
+                </div>
+
+                <Testimonial/>
+            </div>
+        </section>
+
+        {/* =========== Testimonial End =========== */}
     </>
     );   
 };
